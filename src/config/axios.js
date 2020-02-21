96bda403bd0744c487407eb683fc5a3d26245378
juanseromo12 <juanseromo12@gmail.com>
2020-02-20 23:57:03 -0500

    change routes

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
-const port = process.env.PORT || 4000;
 const axiosClient = axios.create({
-    baseURL: process.env.REACT_APP_BACKEND_URL + `:${port}`
+    baseURL: process.env.REACT_APP_BACKEND_URL
 });
 
 export default axiosClient;
