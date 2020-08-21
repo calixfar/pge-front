0390aafbd00ecb64d3472d59d8f5e5d43e595881
luis78 <luis@jumpdigital.co>
2020-08-20 21:53:50 -0500

    Change url back

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
+console.log(process.env.REACT_APP_BACKEND_URL_PROD);
 const axiosClient = axios.create({
-    baseURL: process.env.REACT_APP_BACKEND_URL_DEV
+    baseURL: process.env.REACT_APP_BACKEND_URL_PROD
 });
 
 export default axiosClient;
